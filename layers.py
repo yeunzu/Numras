de21a88a67717cf1006d73f77abf2460adc88abc
yeunzu <yj060706@kakao.com>
2026-05-06 21:55:49 +0900

    second commit

--- layers.py
+++ layers.py
@@ -217,3 +217,192 @@ class ResidualBlock:
     def update(self):
         for layer in self.internal_layers:
             layer.update()
+
+
+class Affine_ResNet:
+    def __init__(self, initializer=He, optimizer=SGD, learning_rate=0.01, size=None):
+        self.affine1 = Affine(input_size=size, output_size=size, initializer=initializer, optimizer=optimizer, learning_rate=learning_rate)
+        self.relu1 = Relu()
+        self.affine2 = Affine(input_size=size, output_size=size, initializer=initializer, optimizer=optimizer, learning_rate=learning_rate)
+        self.internal_layers = [self.affine1, self.affine2]
+
+    def forward(self, x):
+        self.x = x
+
+        out = self.affine1.forward(x)
+        out = self.relu1.forward(out)
+        out = self.affine2.forward(out)
+
+        return out + self.x
+
+    def backward(self, dout):
+        dx_main = self.affine2.backward(dout)
+        dx_main = self.relu1.backward(dx_main)
+        dx_main = self.affine1.backward(dx_main)
+
+        return dx_main + dout
+
+    def update(self):
+        for layer in self.internal_layers:
+            layer.update()
+
+
+class BatchNormalization:
+    def __init__(self, gamma=1.0, beta=0.0, momentum=0.9, running_mean=None, running_var=None, optimizer=SGD, learning_rate=0.01):
+        self.gamma = gamma # 스케일(scale) 파라미터
+        self.beta = beta   # 시프트(shift) 파라미터
+        self.momentum = momentum
+        self.input_shape = None # Conv 레이어 대응용 (4차원의 경우)
+
+        self.dgamma = np.zeros_like(self.gamma)
+        self.dbeta = np.zeros_like(self.beta)
+
+        # 시험 때 사용할 이동 평균 및 분산
+        self.running_mean = running_mean
+        self.running_var = running_var  
+        
+        # 역전파 시 사용할 중간 데이터
+        self.batch_size = None
+        self.xc = None
+        self.std = None
+        self.xn = None
+        
+        # 파라미터 및 기울기
+        self.params = [self.gamma, self.beta]
+        self.grads = [self.dgamma, self.dbeta]
+
+        self.train_flg = True
+        
+        self.optimizer = optimizer(lr=learning_rate)
+
+    def forward(self, x, train_flg=None):
+        if train_flg is None: train_flg=self.train_flg
+        self.input_shape = x.shape
+        if x.ndim != 2:
+            N, C, H, W = x.shape
+            x = x.reshape(N, -1)
+
+        out = self.__forward(x, train_flg)
+        
+        return out.reshape(*self.input_shape)
+            
+    def __forward(self, x, train_flg):
+        if self.running_mean is None:
+            D = x.shape[1]
+            self.running_mean = np.zeros(D)
+            self.running_var = np.zeros(D)
+                        
+        if train_flg:
+            mu = x.mean(axis=0)
+            xc = x - mu
+            var = np.mean(xc**2, axis=0)
+            std = np.sqrt(var + 10e-7)
+            xn = xc / std
+            
+            self.batch_size = x.shape[0]
+            self.xc = xc
+            self.xn = xn
+            self.std = std
+            self.running_mean = self.momentum * self.running_mean + (1 - self.momentum) * mu
+            self.running_var = self.momentum * self.running_var + (1 - self.momentum) * var            
+        else:
+            xc = x - self.running_mean
+            xn = xc / ((np.sqrt(self.running_var + 10e-7)))
+            
+        out = self.gamma * xn + self.beta 
+        return out
+
+    def backward(self, dout):
+        if dout.ndim != 2:
+            N, C, H, W = dout.shape
+            dout = dout.reshape(N, -1)
+
+        dx = self.__backward(dout)
+
+        dx = dx.reshape(*self.input_shape)
+        return dx
+
+    def __backward(self, dout):
+        dbeta = dout.sum(axis=0)
+        dgamma = np.sum(self.xn * dout, axis=0)
+        dxn = self.gamma * dout
+        dxc = dxn / self.std
+        dstd = -np.sum((dxn * self.xc) / (self.std**2), axis=0)
+        dvar = 0.5 * dstd / self.std
+        dxc += (2.0 / self.batch_size) * self.xc * dvar
+        dmu = np.sum(dxc, axis=0)
+        dx = dxc - dmu / self.batch_size
+        
+        self.grads[0] = dgamma
+        self.grads[1] = dbeta
+        
+        return dx
+
+    def update(self):
+        # gamma와 beta 업데이트
+        # 주의: self.params를 [self.gamma, self.beta]로 묶었으므로 리스트 내부 값이 업데이트 되어야 함
+        self.optimizer.update(self.params, self.grads)
+
+
+class ResidualBlock_With_Norm:
+    def __init__(self, filter_num, filter_height, filter_width, stride=1, pad=1, initializer=He, optimizer=SGD, lr=0.01, train_flg=True):
+        # 블록 내부에 사용할 레이어 조립
+        # 입력 채널과 출력 채널이 같다고 가정할 때의 구성
+        self.train_flg = train_flg
+        self.conv1 = Conv(filter_num, filter_num, filter_height, filter_width, stride, pad, initializer=initializer, optimizer=optimizer, learning_rate=lr)
+        self.bn1 = BatchNormalization(learning_rate=lr)
+        self.relu1 = Relu()
+        self.conv2 = Conv(filter_num, filter_num, filter_height, filter_width, 1, pad, initializer=initializer, optimizer=optimizer, learning_rate=lr)
+        self.bn2 = BatchNormalization(learning_rate=lr)
+        self.internal_layers = [self.conv1, self.bn1, self.conv2, self.bn2] # 가중치 업데이트 위해 레이어 리스트로 묶기
+
+    def forward(self, x):
+        self.bn1.train_flg = self.train_flg
+        self.bn2.train_flg = self.train_flg
+        self.x = x # 지름길로 보낼 입력값 보관
+
+        # 메인 경로 연산
+        out = self.conv1.forward(x)
+        out = self.bn1.forward(out)
+        out = self.relu1.forward(out)
+        out = self.conv2.forward(out)
+        out = self.bn2.forward(out)
+
+        # 지름길 더하기(F(x)+x)
+        return out + self.x
+
+    def backward(self, dout):
+        # 메인 경로 역전파
+        dx_main = self.bn2.backward(dout)
+        dx_main = self.conv2.backward(dx_main)
+        dx_main = self.relu1.backward(dx_main)
+        dx_main = self.bn1.backward(dx_main)
+        dx_main = self.conv1.backward(dx_main)
+
+        # 최종 입력값 x로 흐르는 기울기 = (메인 경로 기울기 + 지름길 기울기)
+        return dx_main + dout
+
+    def update(self):
+        for layer in self.internal_layers:
+            layer.update()
+
+
+class Dropout:
+    def __init__(self, dropout_ratio=0.5, train_flg=True):
+        self.dropout_ratio = dropout_ratio
+        self.mask = None
+        self.train_flg = train_flg
+
+    def forward(self, x, train_flg=None):
+        if train_flg == None: train_flg = self.train_flg
+        if train_flg:
+            self.mask = np.random.rand(*x.shape) > self.dropout_ratio
+            return x * self.mask
+        else:
+            return x * (1.0 - self.dropout_ratio)
+        
+    def backward(self, dout):
+        return dout * self.mask
+    
+    def update(self, *args):
+        pass
